52abf2ad444e23f65d9c9e6968ab90df1c97e239
Felipe Cavalca <flpsnocvla@gmail.com>
2024-09-01 23:34:08 -0300

    Modificando campos timestamp para timestamptz

--- database/install/user.sql
+++ database/install/user.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Exemplo para criar um trigger para a tabela 'users'
