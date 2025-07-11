47c0b3349058eddfb821c2f39ecfb60a658014b3
Felipe Cavalca <contato@felipecavalca.dev>
2025-07-10 21:32:35 -0300

    Refatora scripts de inicialização e atualização: adiciona logs informativos e remove script de instalação obsoleto

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -5,15 +5,12 @@ set -e
 docker-entrypoint.sh postgres &
 
 # Aguarda o PostgreSQL estar pronto
-echo "Aguardando o PostgreSQL iniciar..."
+echo "[BFR][Info] Aguardando o PostgreSQL iniciar..."
 until pg_isready -h localhost -p 5432 -U postgres; do
   sleep 2
 done
 
-echo "PostgreSQL iniciado. Executando scripts SQL..."
-
-# Executa o script de instalação
-bash /scripts/install.sh
+echo "[BFR][Info] PostgreSQL iniciado. Executando scripts SQL..."
 
 # Executa o script de atualização
 bash /scripts/update.sh
